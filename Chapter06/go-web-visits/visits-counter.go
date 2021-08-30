ad0eb59b20d97265e3b49745eb47dabd3594ae1c
red <red54321@outlook.com>
2021-08-30 15:38:59 +0100

    Add showing private IP
    This is done so that if this app is run in an Auto scaling group behind a load balancer, one can see that responses come from different servers.

--- Chapter06/go-web-visits/visits-counter.go
+++ Chapter06/go-web-visits/visits-counter.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"log"
+	"net"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -25,12 +26,19 @@ func check(err error) {
 	}
 }
 
+type Info struct {
+	Counter int64
+	Ip      string
+}
+
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	visitsCounter := updateDbCounter()
-	templateText := "<h1>By Red:</h1>\n\nVisits Counter: {{.}}\n"
-	tmpl, err := template.New("count").Parse(templateText)
+	info := Info{visitsCounter, GetLocalIP()}
+
+	templateText := "<h1>By Red:</h1>\n\nVisits Counter: {{.Counter}}\n\nPrivate IP: {{.Ip}}\n"
+	t, err := template.New("count").Parse(templateText)
 	check(err)
-	err = tmpl.Execute(writer, visitsCounter)
+	err = t.Execute(writer, info)
 	check(err)
 }
 
@@ -70,4 +78,21 @@ func dbConn() (db *sql.DB) {
 		panic(err.Error())
 	}
 	return db
+}
+
+// GetLocalIP returns the non loopback local IP of the host
+func GetLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
 }
